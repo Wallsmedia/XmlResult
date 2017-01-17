785698ae91dbe1a00f8031bc0d7e4f00e8660272
Alexander Paskhin <paskhin@hotmail.co.uk>
2017-01-17 23:57:04 +0000

    Added sample of the controller

---
+++ AspNetCore.Mvc.Ver1.1.0/samples/WebXmlResultVS2015/Controllers/XmlExtController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebXmlResultVS2015.Controllers.Models;
+using Microsoft.AspNetCore.Mvc.Formatters.Xml.Extensions;
+
+namespace WebXmlResultVS2015.Controllers
+{
+    [Route("api/[controller]")]
+    public class XmlExtController : Controller
+    {
+        // GET api/[controller]/xml
+        [HttpGet("xml")]
+        public ActionResult GetXmlObject()
+        {
+            object obj = new PurchaseOrder();
+            return new XmlResult(obj);
+        }
+
+        // GET api/[controller]/dcxml
+        [HttpGet("dcxml")]
+        public ActionResult GetDcXmlObject()
+        {
+            object obj = new PurchaseOrder();
+            return new XmlResult(obj) { XmlSerializerType = XmlSerializerType.DataContractSerializer };
+        }
+
+        // POST api/[controller]/xml
+        [HttpPost("xml")]
+        public void PostXml([FromXmlBody]PurchaseOrder value)
+        {
+            var x = value;
+            x.billTo.street += " 123";
+        }
+
+        // POST api/[controller]/dcxml
+        [HttpPost("dcxml")]
+        public void PostDcXml([FromXmlBody(XmlSerializerType = XmlSerializerType.DataContractSerializer)]PurchaseOrder value)
+        {
+            var x = value;
+            x.billTo.street += "No -10";
+        }
+
+    }
+}
