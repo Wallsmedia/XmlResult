518f72f7f01b2f7ec66d6a61f29c85fc60233cfb
Alexander Paskhin <paskhin@hotmail.co.uk>
2017-02-20 20:12:54 +0000

    Add draft release for version Mvc 1.2.0

---
+++ AspNetCore.Mvc.Ver1.2.0/src/Microsoft.AspNetCore.Mvc.Formatters.Xml.Extensions/DependencyInjection/MvcXmlMvcBuilderExtensions.cs
@@ -0,0 +1,51 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters.Xml.Internal;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// Extension methods for adding Xml formatters to MVC.
+    /// </summary>
+    public static class MvcXmlMvcBuilderExtensions
+    {
+        /// <summary>
+        /// Adds the XmlSerializer and DataContractSerializer formatters to MVC.
+        /// Adds the XmlResult and FromXmlBody Extension to MVC.
+        /// </summary>
+        /// <param name="builder">The <see cref="IMvcBuilder"/>.</param>
+        /// <returns>The <see cref="IMvcBuilder"/>.</returns>
+        public static IMvcBuilder AddXmlFormaterExtensions(this IMvcBuilder builder)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            AddXmlFormaterExtensionsServices(builder.Services);
+            return builder;
+        }
+
+        // Internal for testing.
+        internal static void AddXmlFormaterExtensionsServices(IServiceCollection services)
+        {
+            services.TryAddEnumerable(
+                ServiceDescriptor.Transient<IConfigureOptions<MvcOptions>, MvcXmlDataContractSerializerMvcOptionsSetup>());
+            services.TryAddSingleton<XmlDcResultExecutor>();
+            services.TryAddTransient<DcXmlBodyModelBinder>();
+            services.TryAddTransient<DcXmlBodyModelBinderOnly>();
+
+            services.TryAddEnumerable(
+                ServiceDescriptor.Transient<IConfigureOptions<MvcOptions>, MvcXmlSerializerMvcOptionsSetup>());
+            services.TryAddSingleton<XmlResultExecutor>();
+            services.TryAddTransient<XmlBodyModelBinder>();
+            services.TryAddTransient<XmlBodyModelBinderOnly>();
+        }
+    }
+}
---
+++ AspNetCore.Mvc.Ver1.2.0/src/Microsoft.AspNetCore.Mvc.Formatters.Xml.Extensions/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Reflection;
+using System.Resources;
+
+[assembly: AssemblyMetadata("Serviceable", "True")]
+[assembly: NeutralResourcesLanguage("en-us")]
+[assembly: AssemblyCompany("Wallsmedia LTD")]
+[assembly: AssemblyCopyright("2017-* © Wallsmedia LTD. All rights reserved.")]
+[assembly: AssemblyProduct("Extension of Microsoft ASP.NET Core")]
