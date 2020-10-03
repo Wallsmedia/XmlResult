a89dbeb1416c90e8d1c093df5be37c1e684c1faa
AlexPaskhin <paskhin@hotmail.co.uk>
2020-10-03 14:10:35 +0100

    Add headers to files

--- Mvc.Formatters.Xml.Extensions/samples/WebXmlResult/Models/PurchaseOrder.cs
+++ Mvc.Formatters.Xml.Extensions/samples/WebXmlResult/Models/PurchaseOrder.cs
@@ -1,4 +1,7 @@
-﻿using System.Runtime.Serialization;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Runtime.Serialization;
 
 namespace WebXmlResultVS2015.Controllers.Models
 {
